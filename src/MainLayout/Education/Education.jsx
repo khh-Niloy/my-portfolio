695abc410f844c7e076e1f7688db52027c36f285
khh-Niloy <niloy.dev.101@gmail.com>
2025-02-05 21:24:53 +0600

    removed PH from education

--- src/MainLayout/Education/Education.jsx
+++ src/MainLayout/Education/Education.jsx
@@ -59,7 +59,7 @@ const Education = () => {
 
             <div className="md:w-[50%] w-[80%] mx-auto flex mt-10 relative">
               <div className="w-3 h-3 bg-[#b86742c5] rounded-full absolute top-0 xl:right-[7.6rem] lg:right-[6.1rem] right-[8.2rem] sm:right-[12.45rem] md:right-[4.9rem]"></div>
-              <div className="w-3 h-3 bg-[#b86742c5] rounded-full absolute top-[6rem] xl:right-[7.6rem] lg:right-[6.1rem] right-[8.2rem] sm:right-[12.45rem] md:right-[4.9rem]"></div>
+              {/* <div className="w-3 h-3 bg-[#b86742c5] rounded-full absolute top-[6rem] xl:right-[7.6rem] lg:right-[6.1rem] right-[8.2rem] sm:right-[12.45rem] md:right-[4.9rem]"></div> */}
               <div className="w-1/2 border-r-[1.4px] border-[rgba(210,180,140,0.3)] flex flex-col items-start">
                 <h1 className="text-xs text-left text-[#1E293B] leading-[1.4] -translate-x-4">
                   BSc in Software Engineering, <br /> Daffodil International
@@ -70,14 +70,14 @@ const Education = () => {
                 </p>
               </div>
 
-              <div className="w-1/2 text-left">
+              {/* <div className="w-1/2 text-left">
                 <h1 className="text-xs translate-x-5 text-[#1E293B] whitespace-nowrap mt-[5.9rem]">
                   Web Development Course, <br /> Programming Hero
                 </h1>
                 <p className="text-[11px] mt-2 text-[#1E293B]/50 translate-x-5">
                   2024 - Present
                 </p>
-              </div>
+              </div> */}
             </div>
           </motion.div>
 
